8b55fe9625ef1aa6d7dd3a6c7e5d436cd712eb47
Shaker Kattoula <smkattoula@gmail.com>
2020-08-06 15:28:36 -0700

    create HTML markup and create speech boxes

---
+++ script.js
@@ -0,0 +1,74 @@
+const main = document.querySelector("main");
+const voicesSelect = document.getElementById("voices");
+const textArea = document.getElementById("text");
+const readBtn = document.getElementById("read");
+const toggleBtn = document.getElementById("toggle");
+const closeBtn = document.getElementById("close");
+
+const data = [
+  {
+    image: "./img/drink.jpg",
+    text: "I am thirsty",
+  },
+  {
+    image: "./img/angry.jpg",
+    text: "I am angry",
+  },
+  {
+    image: "./img/food.jpg",
+    text: "I am hungry",
+  },
+  {
+    image: "./img/grandma.jpg",
+    text: "I want to go to grandma's house",
+  },
+  {
+    image: "./img/happy.jpg",
+    text: "I am happy",
+  },
+  {
+    image: "./img/home.jpg",
+    text: "I want to go home",
+  },
+  {
+    image: "./img/hurt.jpg",
+    text: "I am hurt",
+  },
+  {
+    image: "./img/outside.jpg",
+    text: "I want to go outside",
+  },
+  {
+    image: "./img/sad.jpg",
+    text: "I am sad",
+  },
+  {
+    image: "./img/scared.jpg",
+    text: "I am scared",
+  },
+  {
+    image: "./img/school.jpg",
+    text: "I want to go to school",
+  },
+  {
+    image: "./img/tired.jpg",
+    text: "I am tired",
+  },
+];
+
+data.forEach(createBox);
+
+// Create a function that creates speech boxes
+function createBox(item) {
+  const box = document.createElement("div");
+
+  const { image, text } = item;
+
+  box.classList.add("box");
+  box.innerHTML = `<img src="${image}" alt="${text}" />
+  <p class="info">${text}</p>`;
+
+  // Speak event
+
+  main.appendChild(box);
+}
